ad13a4e02d1b7dcef4e944773630226947d202ba
yavinash249 <y.avinash249@gmail.com>
2025-01-29 11:17:45 +0530

    add image and project

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -44,7 +44,7 @@ const About = () => {
           >
             <div className="aspect-square rounded-2xl overflow-hidden">
               <img
-                src="https://images.unsplash.com/photo-1587620962725-abab7fe55159?q=80&w=1931&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" // Add your image path
+                src="/src/assets/PROFILE.jpg" // Add your image path
                 alt="Profile"
                 className="w-full h-full object-cover"
               />
